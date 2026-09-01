4fcd9e28307d4c3a3bc82c03123442a93fc5c304
Arvid Sundqvist <carlarvidsundqvist@gmail.com>
2026-09-01 13:39:33 +0200

    refactor: co-locate group builtin

--- src/renderer/builtins/group/index.tsx
+++ src/renderer/builtins/group/index.tsx
@@ -1,6 +1,6 @@
 import type { ReactNode } from "react";
-import type { ComponentRendererProps } from "./types";
-import { childSurface } from "./shared";
+import type { ComponentRendererProps } from "../types";
+import { childSurface } from "../shared";
 
 export default function Group({ children }: ComponentRendererProps): ReactNode {
   return childSurface(children);
