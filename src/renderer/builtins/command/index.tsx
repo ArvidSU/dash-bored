406d02fdc40a69a0427087298fa2042a6e40afd6
Arvid Sundqvist <carlarvidsundqvist@gmail.com>
2026-09-01 13:39:12 +0200

    refactor: co-locate command builtin

--- src/renderer/builtins/command/index.tsx
+++ src/renderer/builtins/command/index.tsx
@@ -2,8 +2,8 @@ import { useEffect, useRef, useState } from "react";
 import type { ReactNode } from "react";
 import { Terminal as XtermTerminal } from "@xterm/xterm";
 import "@xterm/xterm/css/xterm.css";
-import type { ComponentRendererProps } from "./types";
-import { CapabilityGate, stringProp } from "./shared";
+import type { ComponentRendererProps } from "../types";
+import { CapabilityGate, stringProp } from "../shared";
 
 export default function Command({
   props,
