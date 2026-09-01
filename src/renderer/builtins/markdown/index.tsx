e6b7b1c83b34a3c7c59f87eab57432aa96addc3a
Arvid Sundqvist <carlarvidsundqvist@gmail.com>
2026-09-01 13:41:25 +0200

    refactor: co-locate markdown builtin

--- src/renderer/builtins/markdown/index.tsx
+++ src/renderer/builtins/markdown/index.tsx
@@ -1,9 +1,9 @@
 import type { ReactNode } from "react";
 import ReactMarkdown from "react-markdown";
 import "./markdown.css";
-import type { ComponentRendererProps } from "./types";
-import { stringProp } from "./shared";
-import { safeMarkdownUrl } from "../safe-url";
+import type { ComponentRendererProps } from "../types";
+import { stringProp } from "../shared";
+import { safeMarkdownUrl } from "../../safe-url";
 
 export default function Markdown({ props }: ComponentRendererProps): ReactNode {
   const content = stringProp(props, ["content", "markdown"]);
